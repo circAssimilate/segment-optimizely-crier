d4f19b083949853decc49d3ed71d76e018333323
Derek Hammond <derek@scribe.ai>
2021-03-02 09:18:19 -0600

    feat(onTrack): Add Segment Custom Destination Function (#1)
    Not all the PR feedback has been addressed but merging as is since I won't get to this anymore.
    
    This PR will introduce the first version of the Segment Custom Destination Function for Optimizely Full Stack Environments. Check out https://segment.com/docs/connections/destinations/destination-functions/ and all the other documentation in this repo's README.md for more info.

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,29 @@
-const { __fetchProjectData, __uuidv4 } = require('./');
+const { __fetchProjectData, __uuidv4, onTrack } = require('./');
 
-// TODO(derek@optimizely.com) Update tests
+let fetchMock;
+
+beforeEach(() => {
+  fetchMock = {
+    fn: () =>
+      new Promise(resolve =>
+        resolve({
+          json: () =>
+            new Promise(resolve =>
+              resolve({
+                accountId: '123456',
+                attributes: [{ id: '9876', key: 'attr_key_one' }],
+                events: [{ id: '5432', key: 'event_key_one' }],
+              })
+            ),
+        })
+      ),
+  };
+});
+
+// TODO(derek@optimizely.com) Add tests
+describe('onTrack', () => {});
+
+// TODO(derek@optimizely.com) Add tests
 describe('__fetchProjectData', () => {
   it('fails as expected with no SDK Key', () => {});
   it('fails as expected with invalid SDK Key', () => {});
